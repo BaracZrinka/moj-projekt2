18448d533e8ebc79ba22fb7210e766cda6a3ae21
{zrinka <{zrinka.barac1@gmail.com}>
2020-10-21 23:24:04 +0200

    adding header contact bar

---
+++ src/components/ContactBar/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './style.module.css'
+
+const ContactBar = () => (
+    <main className = {styles.container}>
+        <span>zrinka.barac1@gmail.com</span>
+        <span>099588888</span>
+    </main>
+)
+export default ContactBar
